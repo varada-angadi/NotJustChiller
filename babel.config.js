33dc5b7f1895d0fd2a97562114462b9398b771fb
Varada2806 <107529891+Varada2806@users.noreply.github.com>
2025-09-16 02:53:08 +0530

    Update project: assets

--- babel.config.js
+++ babel.config.js
@@ -5,13 +5,13 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: 
-    ['module:react-native-dotenv', {
+    plugins: [
+      ["module:react-native-dotenv", {
         moduleName: '@env',
         path: '.env',
         safe: false,
         allowUndefined: true
       }]
-    
+    ]
   };
 };
